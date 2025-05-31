3ad7f264a70f9346dc63583b3db4966e2e8603ed
Francis Loyd Raval <jasmarket577@gmail.com>
2025-05-31 17:49:39 +0800

    Update test.ts

--- totoro/modules/commands/test.ts
+++ totoro/modules/commands/test.ts
@@ -1,9 +1,8 @@
-export default {
-  meta: {
+export default meta = {
     name: 'test',
     aliases: ['t'],
   },
   execute: async ({ response, event }) => {
     await response.send('Test command executed!', event.threadID);
   },
-};
+
