c78af5b5b6a4a1810acb7a891844ca8534d4a8d6
Francis Loyd Raval <jasmarket577@gmail.com>
2025-05-31 17:37:30 +0800

    Update test.ts

--- totoro/modules/commands/test.ts
+++ totoro/modules/commands/test.ts
@@ -1,19 +1,9 @@
 export default {
   meta: {
-    name: 'ping',
-    aliases: ['p'],
-    category: 'utility',
-    description: 'Checks if the bot is responsive',
-    developer: 'Francis Loyd Raval',
-    usage: '!ping [optional message]',
-    role: 0,
-    cooldown: 5,
+    name: 'test',
+    aliases: ['t'],
   },
-  async execute({ response, args, event }) {
-    const message = args.length > 0 ? args.join(' ') : 'Pong!';
-    const info = await response.send(`🏓 ${message}`);
-    if (!info) {
-      await response.send('Failed to send ping response.');
-    }
+  execute: async ({ response, event }) => {
+    await response.send('Test command executed!', event.threadID);
   },
 };
