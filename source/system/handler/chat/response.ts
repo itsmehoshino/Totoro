9342a41e5872e5ef7eec9e33968f0151ce51ca2a
Francis Loyd Raval <jasmarket577@gmail.com>
2025-06-01 13:01:55 +0800

    Update response.ts

--- source/system/handler/chat/response.ts
+++ source/system/handler/chat/response.ts
@@ -1,65 +1,35 @@
 import { promisify } from 'util';
 
-export function createResponse(api, event) {
-  const chatBox = {
-    react: (emoji: string) => api.setMessageReaction(emoji, event.messageID, () => {}),
-    send: (message: string, id?: string) => api.sendMessage(message, id || event.threadID, event.messageID),
-    addParticipant: (uid: string) => api.addUserToGroup(uid, event.threadID),
-    removeParticipant: (uid: string) => api.removeUserFromGroup(uid, event.threadID),
-    threadInfo: async () => await api.getThreadInfo(event.threadID),
-  };
+export class Response {
+  constructor(api, event) {
+    this.api = api;
+    this.event = event;
+  }
 
-  const response = {
-    ...chatBox,
-    send: (message: string, goal?: string): Promise<boolean> => {
-      return new Promise((res, rej) => {
-        api.sendMessage(message, goal || event.threadID, (err: any) => {
-          if (err) rej(err);
-          else res(true);
-        });
+  send(message, goal) {
+    return new Promise((res, rej) => {
+      this.api.sendMessage(message, goal || this.event.threadID, (err) => {
+        if (err) rej(err);
+        else res(true);
       });
-    },
-    reply: (message: string, goal?: string): Promise<boolean> => {
-      return new Promise((res, rej) => {
-        api.sendMessage(
-          { body: message, replyToMessage: event.messageID },
-          goal || event.threadID,
-          (err: any) => {
-            if (err) rej(err);
-            else res(true);
-          }
-        );
-      });
-    },
-    edit: (msg: string, mid: string): Promise<boolean> => {
-      return new Promise((res, rej) => {
-        api.editMessage(msg, mid, (err: any) => {
-          if (err) rej(err);
-          else res(true);
-        });
-      });
-    },
-    fbPost: async ({ body, attachment }: { body?: string; attachment?: any[] }): Promise<any> => {
-      return new Promise((resolve, reject) => {
-        api.createPost({ body: body || '', attachment: attachment || [] }, (error: any, data: any) => {
-          if (error) {
-            console.error('Facebook Post Error:', error);
-            reject({ success: false, message: 'Failed to create post', error });
-            return;
-          }
+    });
+  }
 
-          if (!data?.data || data.errors) {
-            console.error('Unexpected API Response:', data);
-            reject({ success: false, message: 'API returned an error', data });
-            return;
-          }
-
-          console.log('Post Successful:', data);
-          resolve({ success: true, data });
-        });
+  reply(message, goal) {
+    return new Promise((res, rej) => {
+      this.api.sendMessage(message, this.event.messageID, goal || this.event.threadID, (err) => {
+        if (err) rej(err);
+        else res(true);
       });
-    },
-  };
+    });
+  }
 
-  return response;
+  edit(msg, mid) {
+    return new Promise((res, rej) => {
+      this.api.editMessage(msg, mid, (err) => {
+        if (err) rej(err);
+        else res(true);
+      });
+    });
+  }
 }
