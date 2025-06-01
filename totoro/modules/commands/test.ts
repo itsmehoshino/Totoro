4b80dc493d7ce5690a3225fa39739f7322da31e8
Francis Loyd Raval <jasmarket577@gmail.com>
2025-06-01 11:11:06 +0800

    Delete totoro/modules/commands/test.ts

--- totoro/modules/commands/test.ts
+++
@@ -1,8 +0,0 @@
-export default meta = {
-    name: 'test',
-    aliases: ['t'],
-  };
-  execute: async ({ response, event }) => {
-    await response.send('Test command executed!', event.threadID);
-  };
-
