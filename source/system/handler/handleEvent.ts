9da953eac3640594c4804801ba2cc8d7f58bbc3d
Francis Loyd Raval <jasmarket577@gmail.com>
2025-06-13 19:39:25 +0800

    Update handleEvent.ts

--- source/system/handler/handleEvent.ts
+++ source/system/handler/handleEvent.ts
@@ -1,3 +1,22 @@
-export async function handleEvent({ api, event }){
-  
+export async function handleEvent({ ...entryObj }: { [key: string]: any }) {
+  const { events } = global.Totoro as { events: Map<any, { meta: { name: string }; onEvent: Function }> };
+  const { api, event, response } = entryObj;
+
+  try {
+    for (const { meta, onEvent } of events.values()) {
+      if (event && meta.name) {
+        const args = event.body?.split("");
+        try {
+          await onEvent({
+            ...entryObj,
+            args,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
